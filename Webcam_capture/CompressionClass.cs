ced7408277d36011bfdb5d41039e05fcfe54d624
Chrislilja <christopher.thomas.lilja@gmail.com>
2018-01-28 12:25:11 -0800

    First_Commit

---
+++ Webcam_capture/CompressionClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webcam_capture
+{
+    class CompressionClass
+    {
+
+        public Image tester(Image Image_to_compress)
+        {
+
+
+            Bitmap convertedtobitmap = new Bitmap(Image_to_compress);
+
+         
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                // Create an Encoder object based on the GUID  
+                // for the Quality parameter category.  
+                System.Drawing.Imaging.Encoder myEncoder =
+                    System.Drawing.Imaging.Encoder.Quality;
+
+                // Create an EncoderParameters object.  
+                // An EncoderParameters object has an array of EncoderParameter  
+                // objects. In this case, there is only one  
+                // EncoderParameter object in the array.  
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 10L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+            //convertedtobitmap.Save(@"c:\TestPhotoQualityFifty.jpg", jpgEncoder, myEncoderParameters);
+
+            var ms = new MemoryStream();
+            convertedtobitmap.Save(ms, jpgEncoder, myEncoderParameters);
+
+
+            
+            return Bitmap.FromStream(ms);
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ Webcam_capture/Form1.cs
@@ -0,0 +1,68 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webcam_capture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public FilterInfoCollection CamsCollection;
+        public VideoCaptureDevice Cam = null;
+        public Bitmap currimg;
+        CompressionClass CC = new CompressionClass();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            CamsCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+           // MessageBox.Show(Convert.ToString(videoDevices.Count));
+           
+            Cam = new VideoCaptureDevice(CamsCollection[0].MonikerString);
+
+
+            Cam = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            Cam.NewFrame += new NewFrameEventHandler(Cam_NewFrame);
+            Cam.Start();
+
+        }
+
+        private void Cam_NewFrame(object sender,
+        NewFrameEventArgs eventArgs)
+        {
+            
+            // get new frame
+            currimg = eventArgs.Frame;
+            // process the frame
+            Image leimage = (Image)eventArgs.Frame.Clone();
+            
+            pictureBox1.Image = CC.tester(leimage); ;
+           // currimg.Dispose();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cam.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
